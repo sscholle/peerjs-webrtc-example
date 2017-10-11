c5012a6aa4a831671579872b1a2528e32baa7752
Sebastian Scholle <sebastian.scholle@derivco.co.za>
2017-10-11 10:56:34 +0200

    Updated client bundler (work in progress) global peerjs config for signalling server settings Cleaned up code and correctly decode ArrayBuffer to string

--- server.js
+++ server.js
@@ -1,40 +1,31 @@
+var peerjsConfig = require("./peerjsConfig.js");
+var clientName = 'node-client';
 window = global
 window.BlobBuilder = require("BlobBuilder")
 location = {protocol: 'http'}
-
-// BinaryPack = require("binary-pack");
-// XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 BinaryPack = require("binary-pack")
 XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-//var wrtc = require('electron-webrtc')()
-
-
 var wrtc = require("wrtc");
 RTCPeerConnection = wrtc.RTCPeerConnection;
 RTCSessionDescription = wrtc.RTCSessionDescription;
 RTCIceCandidate = wrtc.RTCIceCandidate;
-
 WebSocket = require('ws');
-
 require('./node_modules/peerjs/lib/exports.js');
 
-var peer = new Peer('basic-peer-server',  {host: 'localhost', port: 9000, path: '/', debug: true});
+var peer = new Peer(clientName, peerjsConfig);
 
 peer.on('open', function () {
   console.log('peer open');
   //
   // var conn = peer.connect('basic-client-peer');
-
-
-
 });
 
 peer.on('connection', function(conn) {
-    conn.serialization='none'
+    conn.serialization='none' // Note: this circumvents Blob/BlobBuilder in Node - but it may not be very efficient - to investigate
 
     conn.on('data', function(data) {
       //TODO Decode actual array buffer data --- Buffer.from(data);
-      console.log('Received', data);
+      console.log('Received', data, Buffer.from(data, 1).toString());
     });
 
     conn.on('error', function (err) {
